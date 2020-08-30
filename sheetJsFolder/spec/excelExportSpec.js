ac80c515fe54b8015b98560120188e1157e7e196
dgkim43 <timekillman@gmail.com>
2020-08-31 07:53:17 +0900

    파일생성

--- sheetJsFolder/spec/excelExportSpec.js
+++ sheetJsFolder/spec/excelExportSpec.js
@@ -1,5 +1,86 @@
+var excelHandler = {
+    getExcelFileName : function(){
+        return 'aoa-test.xlsx';
+    },
+    getSheetName : function(){
+        return 'AOA Test Sheet';
+    },
+    getExcelData : function(){
+        return [['이름' , '나이', '부서'],['도사원' , '21', '인사팀'],['김부장' , '27', '비서실'],['엄전무' , '45', '기획실']];
+    },
+    getWorksheet : function(){
+        return XLSX.utils.aoa_to_sheet(this.getExcelData());
+    }
+}
+
+function s2ab(s) { 
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf);  //create uint8array as viewer
+    for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;    
+}
+
+function handleExcelDataAll(sheet){
+	handleExcelDataHeader(sheet); // header 정보 
+	handleExcelDataJson(sheet); // json 형태
+	handleExcelDataCsv(sheet); // csv 형태
+	handleExcelDataHtml(sheet); // html 형태
+}
+function handleExcelDataHeader(sheet){
+    var headers = get_header_row(sheet);
+    console.log(JSON.stringify(sheet));
+    $("#displayHeaders").html(JSON.stringify(headers));
+}
+function handleExcelDataJson(sheet){
+    console.log(JSON.stringify(XLSX.utils.sheet_to_json (sheet)));
+    $("#displayExcelJson").html(JSON.stringify(XLSX.utils.sheet_to_json (sheet)));
+}
+function handleExcelDataCsv(sheet){
+    $("#displayExcelCsv").html(XLSX.utils.sheet_to_csv (sheet));
+}
+function handleExcelDataHtml(sheet){
+    
+    $("#displayExcelHtml").html(XLSX.utils.sheet_to_html (sheet));
+}
+
+// 출처 : https://github.com/SheetJS/js-xlsx/issues/214
+function get_header_row(sheet) {
+    var headers = [];
+    var range = XLSX.utils.decode_range(sheet['!ref']);
+    var C, R = range.s.r; /* start in the first row */
+    /* walk every column in the range */
+    for(C = range.s.c; C <= range.e.c; ++C) {
+        var cell = sheet[XLSX.utils.encode_cell({c:C, r:R})] /* find the cell in the first row */
+
+        var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
+        if(cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+
+        headers.push(hdr);
+    }
+    return headers;
+}
+
 describe('excelExportSpec', () => {
     describe('excelExport', () => {
+        var wbBlob;
+        beforeEach(() => {
+            // step 1. workbook 생성
+            var wb = XLSX.utils.book_new();
+
+            // step 2. 시트 만들기 
+            var newWorksheet = excelHandler.getWorksheet();
+
+            // step 3. workbook에 새로만든 워크시트에 이름을 주고 붙인다.  
+            XLSX.utils.book_append_sheet(wb, newWorksheet, excelHandler.getSheetName());
+
+            // step 4. 엑셀 파일 만들기 
+            var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+            wbBlob = new Blob([s2ab(wbout)], { type: "application/octet-stream" });
+            // step 5. 엑셀 파일 내보내기
+            //saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), excelHandler.getExcelFileName());
+        })
+
         it('ExcelExport() excelStateData가 없으면 에러를 발생한다.', () => {
             var actual = function () {
                 ExcelExport();
@@ -8,15 +89,31 @@ describe('excelExportSpec', () => {
         });
 
         it('ExcelExport() fileEl가 없으면 에러를 발생한다.', () => {
-            
+
             var actual = function () {
                 ExcelExport(ExcelStateData);
             }
             expect(actual).toThrowError();
         });
 
-        it('onchange', () => {
+        it('change', () => {
+            var fileEl = document.createElement('input');
+            fileEl.type = "file";
+            var excelProcess = ExcelExport(ExcelStateData, fileEl);
+            spyOn(excelProcess, 'fnTest');
+            const e = new Event('change');
+            fileEl.dispatchEvent(e);
+            expect(excelProcess.fnTest).toHaveBeenCalled();
+        });
+
+        it('parseProcess() 파일 파싱', () => {
+            var fileEl = document.createElement('input');
+            fileEl.type = "file";
+            var excelProcess = ExcelExport(ExcelStateData, fileEl);
+            var initData = excelHandler.getExcelData();
+            var parseData = excelProcess.parseProcess(wbBlob, handleExcelDataAll);
             
+            expect(initData).toBe(parseData);
         });
     });
 });
--- sheetJsFolder/src/excelExport.js
+++ sheetJsFolder/src/excelExport.js
@@ -1,35 +1,58 @@
 var ExcelExport = function (excelStateData, fileEl) {
     if (!excelStateData) throw Error('excelStateData is empty');
     if (!fileEl) throw Error('fileEl is empty');
-
+    
     var excelProcess = {
-        fnFileChange(event) {
-            console.log('excelProcess', 'fnFileChange', 'start');
-            console.log(excelStateData);
-            console.log(excelStateData.isfileLoadingEnd);
-            excelStateData.isfileLoadingEnd = false;
-            var input = event.target;
-            var reader = new FileReader();
-            reader.onload = function () {
-                console.log('reader.onload', 'start');
-                var fileData = reader.result;
-                var wb = XLSX.read(fileData, { type: 'binary' });
-                wb.SheetNames.forEach(function (sheetName) {
-                    var rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
-
-                    // console.log(sheetName, JSON.stringify(rowObj));
-                    if (sheetName == 'Sheet1') {
-                        excelStateData.excelExportData = rowObj;
-                        excelStateData.isfileLoadingEnd = true;
-                    }
-                })
-                console.log('reader.onload', 'end');
-            };
-            reader.readAsBinaryString(input.files[0]);
-            console.log('excelProcess', 'fnFileChange', 'end');
+        fnTest(){
+            console.log('fnTest');
         },
+        parseProcess(file, callback){
+            console.log('file', file);
+            var reader = new FileReader();
+            reader.onload = function(){
+                var fileData = reader.result;
+                var wb = XLSX.read(fileData, {type : 'binary'});
+                var sheetNameList = wb.SheetNames; // 시트 이름 목록 가져오기 
+                var firstSheetName = sheetNameList[0]; // 첫번째 시트명
+                var firstSheet = wb.Sheets[firstSheetName]; // 첫번째 시트 
+                callback(firstSheet);      
+            };
+            reader.readAsBinaryString(file);
+        }
     }
-    fileEl.addEventListener('onchange', excelProcess.fnFileChange(event));
+    
+    fileEl.addEventListener('change', e =>{
+        excelProcess.fnTest();
+    });
+
+    // var excelProcess = {
+    //     fnFileChange(event) {
+    //         console.log('excelProcess', 'fnFileChange', 'start');
+    //         console.log(excelStateData);
+    //         console.log(excelStateData.isfileLoadingEnd);
+    //         excelStateData.isfileLoadingEnd = false;
+    //         var input = event.target;
+    //         var reader = new FileReader();
+    //         reader.onload = function () {
+    //             console.log('reader.onload', 'start');
+    //             var fileData = reader.result;
+    //             var wb = XLSX.read(fileData, { type: 'binary' });
+    //             wb.SheetNames.forEach(function (sheetName) {
+    //                 var rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName]);
+
+    //                 // console.log(sheetName, JSON.stringify(rowObj));
+    //                 if (sheetName == 'Sheet1') {
+    //                     excelStateData.excelExportData = rowObj;
+    //                     excelStateData.isfileLoadingEnd = true;
+    //                 }
+    //             })
+    //             console.log('reader.onload', 'end');
+    //         };
+    //         reader.readAsBinaryString(input.files[0]);
+    //         console.log('excelProcess', 'fnFileChange', 'end');
+    //     },
+    // }
+    // fileEl.addEventListener('onchange', excelProcess.fnFileChange(event));
 
     return excelProcess;
 }
